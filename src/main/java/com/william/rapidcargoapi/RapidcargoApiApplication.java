2c8f0d713a4bbc17b1f101794839f78308c935c9
Tab0o33 <w.moinet@orange.fr>
2023-09-28 11:55:33 +0200

    Initial commit

---
+++ src/main/java/com/william/rapidcargoapi/RapidcargoApiApplication.java
@@ -0,0 +1,13 @@
+package com.william.rapidcargoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidcargoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidcargoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/rapidcargoapi/RapidcargoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.rapidcargoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidcargoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
